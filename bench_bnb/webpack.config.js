f19a69b78447fd9de8fa7d6a06246277c19b7699
John Schoeman <johnschoeman1617@gmail.com>
2017-11-22 19:15:43 -0500

    Create user model

--- bench_bnb/webpack.config.js
+++ bench_bnb/webpack.config.js
@@ -1,8 +1,10 @@
+var path = require('path');
+
 module.exports = {
   context: __dirname,
-  entry: "./frontend/index.jsx",
+  entry: "./frontend/bench_bnb.jsx",
   output: {
-    path: 'app/assets/javascripts',
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
   module: {
