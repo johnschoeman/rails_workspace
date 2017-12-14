05366b43f5bc753122496cd3f0743128b028ea7a
John Schoeman <johnschoeman1617@gmail.com>
2017-12-13 20:11:16 -0500

    Annotate models

--- action_cable_chat_app/app/models/message.rb
+++ action_cable_chat_app/app/models/message.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Message < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
